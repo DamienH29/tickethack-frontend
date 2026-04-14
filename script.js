c5fc3af6ac5bb9f347df376f71c03745495b39de
Céline <celineswietlicki@gmail.com>
2026-04-14 18:41:19 +0200

    affichage recherche voyages script.js

--- script.js
+++ script.js
@@ -6,22 +6,31 @@ document.querySelector("#date-input").value = today;
 document.querySelector('#search-button').addEventListener('click', function() {
     const departure = document.querySelector('#departure-input').value;
     const arrival = document.querySelector('#arrival-input').value;
-    fetch(`http://localhost:3000/trips/${departure}/${arrival}`) 
+    const date = document.querySelector('#date-input').value;
+
+    fetch(`http://localhost:3000/trips/${departure}/${arrival}/${date}`) 
         .then(response => response.json())
         .then(data => {
-            document.querySelector("#response-container").innerHTML += `
-            <div class="cityContainer">
-                <p class="name">${data.weather.cityName}</p>
-                <p class="description">${data.weather.decription}</p>
-                <img class="weatherIcon" src="images/${data.weather.main}.png" />
-                <div class="temperature">
-                    <p class="tempMin">${data.weather.tempMin}</p>
-                    <span>-</span>
-                    <p class="tempMax">${data.weather.tempMax}</p>
-                </div>
-                <button class="deleteCity" id=${data.weather.cityName}>Delete</button>
-                </div>
+            document.querySelector("#response-container").innerHTML = ""
+            if(!data.result) {
+                document.querySelector("#response-container").innerHTML += `
+                    <div>
+                        <img src="./images/notfound.png" id="notfound" />
+                        <h4 id="not_found_text">No trip found.</h4>
+                    </div>` 
+            } else {
+                for(let elem of data.trips) {
+                const newDate = elem.date.slice(11, 16)
+
+                document.querySelector("#response-container").innerHTML += `
+                    <div>
+                        <p id="traject-response">${elem.departure}>${elem.arrival}</p>
+                        <p id="hour-response">${newDate}</p>
+                        <p id="price-response">${elem.price}€</p>
+                        <button id="book-button">Book</button>
+                    </div>
             ` 
+        }}     
         })
         .catch(error => console.error('Erreur:', error));
 });
