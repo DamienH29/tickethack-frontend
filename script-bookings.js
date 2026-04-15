db9ec61d2c2b66122995ff05a6d87479521eaa33
Céline <celineswietlicki@gmail.com>
2026-04-15 17:36:45 +0200

    essai js bookings

--- script-bookings.js
+++ script-bookings.js
@@ -1,31 +1,22 @@
-//Add to cart.html + delete button
-fetch("http://localhost:3000/trips/cart")
-  .then((response) => response.json())
-  .then((data) => {
-    data.trips.forEach((trip) => {
-      document.querySelector("#cart-container").innerHTML += `
-            <div class="trip-card">
-                <p>${trip.traject}</p>
-                <p>${trip.hour}</p>
-                <p>${trip.price}</p>
-                <button class="delete-button" id="${trip._id}">X</button>
-            </div>
-        `;
-    });
-    deleteTrip();
-  });
 
-function deleteTrip() {
-  document.querySelectorAll(".delete-button").forEach((button) => {
-    button.addEventListener("click", function () {
-      const id = this.id;
-      fetch(`http://localhost:3000/trips/cart/${id}`, { method: "DELETE" })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.result) {
-            document.querySelector(`#${id}`).parentNode.remove();
-          }
-        });
+fetch('http://localhost:3000/trips/books')
+    .then(response => response.json())
+    .then(data => {
+        if (data.result) {
+            const container = document.querySelector('#bookings-list');
+            container.innerHTML = '<h3>My bookings</h3>';
+
+            for (let trip of data.cart) { 
+                container.innerHTML += `
+                    <div class="booking-item">
+                        <span>${trip.traject}</span>
+                        <span>${trip.hour}</span>
+                        <span>${trip.price}€</span>
+                        <span class="departure">Departure in ...</span>
+                    </div>
+                `;
+            }
+        }
     });
-  });
-}
+
+
--- script-cart.js
+++ script-cart.js
@@ -1,8 +1,12 @@
+let allTripsInCart = []
+
 //Add to cart.html + delete button
 fetch("http://localhost:3000/trips/cart")
   .then((response) => response.json())
   .then((data) => {
     if(data.result) {
+    
+
     document.querySelector("#cart-container").innerHTML = "";
     for(let trip of data.cart) {
       console.log(trip)
@@ -15,8 +19,13 @@ fetch("http://localhost:3000/trips/cart")
             </div>
         `;
     };
+    document.querySelector("#cart-container").innerHTML += `
+        <div id="purchase-section">
+            <button id="purchase-button">Purchase</button>
+        </div>`;
   }
     deleteTrip();
+    addPurchase();
   });
 
 function deleteTrip() {
@@ -32,4 +41,16 @@ function deleteTrip() {
         });
     });
   });
+}
+
+function addPurchase() {
+document.querySelector("#purchase-button").addEventListener("click", function () {
+    fetch("http://localhost:3000/trips/books", { method: "POST" })
+      .then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          window.location.assign("bookings.html");
+        }
+      });
+});
 }
