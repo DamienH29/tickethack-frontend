dace5a5a46031b20728d33c96f8b69678e91efcb
DamienH <d.hamon29@laposte.net>
2026-04-15 10:53:23 +0200

    add script to add to cart and delete

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const today = new Date().toISOString().split('T')[0];
 document.querySelector("#date-input").value = today;
 
 
+// Query all trips from DB that match the search inputs and pull them on index.html
 document.querySelector('#search-button').addEventListener('click', function() {
     const departure = document.querySelector('#departure-input').value;
     const arrival = document.querySelector('#arrival-input').value;
@@ -23,14 +24,70 @@ document.querySelector('#search-button').addEventListener('click', function() {
                 const newDate = elem.date.slice(11, 16)
 
                 document.querySelector("#response-container").innerHTML += `
-                    <div>
-                        <p id="traject-response">${elem.departure}>${elem.arrival}</p>
-                        <p id="hour-response">${newDate}</p>
-                        <p id="price-response">${elem.price}€</p>
-                        <button id="book-button">Book</button>
+                    <div class="trip">
+                        <p class="traject-response">${elem.departure}>${elem.arrival}</p>
+                        <p class="hour-response">${newDate}</p>
+                        <p class="price-response">${elem.price}€</p>
+                        <button class="book-button">Book</button>
                     </div>
             ` 
         }}     
         })
         .catch(error => console.error('Erreur:', error));
 });
+
+
+// Get a trip from the list and add it to the cart list
+document.querySelectorAll(".book-button").forEach(button => {
+    button.addEventListener("click", function() {
+        const trip = {
+            traject: this.closest(".trip-card").querySelector(".traject-response").textContent,
+            hour: this.closest(".trip-card").querySelector(".hour-response").textContent,
+            price: this.closest(".trip-card").querySelector(".price-response").textContent,
+        };
+
+        fetch("http://localhost:3000/cart", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(trip),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.result) {
+                window.location.assign("cart.html");
+            }
+        });
+    });
+});
+
+
+// Add to cart and delete from index.html
+fetch("http://localhost:3000/cart")
+.then(response => response.json())
+.then(data => {
+    data.trips.forEach(trip => {
+        document.querySelector("#cart-container").innerHTML += `
+            <div class="trip-card">
+                <p>${trip.traject}</p>
+                <p>${trip.price}</p>
+                <button class="delete-button" id="${trip._id}">X</button>
+            </div>
+        `;
+    });
+    deleteTrip();
+});
+
+function deleteTrip() {
+    document.querySelectorAll(".delete-button").forEach(button => {
+        button.addEventListener("click", function() {
+            const id = this.id;
+            fetch(`http://localhost:3000/cart/${id}`, { method: "DELETE" })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result) {
+                    document.querySelector(`#${id}`).parentNode.remove();
+                }
+            });
+        });
+    });
+}
