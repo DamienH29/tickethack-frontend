9d517598c5db80a9340caa97c78002c00669e1a4
DamienH <d.hamon29@laposte.net>
2026-04-15 17:00:28 +0200

    add script for cart + style

--- script-cart.js
+++ script-cart.js
@@ -13,30 +13,40 @@ document.querySelector("#bookings").addEventListener("click", function () {
   window.location.assign("bookings.html");
 });
 
-//Add to cart.html + delete button
+//Add to cart.html + delete button + add total
 fetch("http://localhost:3000/trips/cart")
   .then((response) => response.json())
   .then((data) => {
-    if(data.result) {
-    document.querySelector("#cart-container").innerHTML = `
+    let total = 0;
+    if (data.result) {
+      document.querySelector("#cart-container").innerHTML = `
       <p>My cart</p>
       <div id="trip-cards"></div>
       <div id="footer-cart">
-        <p id="cart-total">Total : <span></span>€</p>
+        <p id="cart-total">Total : €</p>
         <button id="purchase-button">Purchase</button>
       </div>
     `;
-    for(let trip of data.cart) {
-      document.querySelector("#trip-cards").innerHTML += `
+      for (let trip of data.cart) {
+        total += parseFloat(trip.price);
+        document.querySelector("#trip-cards").innerHTML += `
             <div class="trip-card">
                 <p>${trip.traject}</p>
                 <p>${trip.hour}</p>
-                <p>${trip.price}</p>
+                <p class="price">${trip.price}</p>
                 <button class="delete-button" id="${trip._id}">X</button>
             </div>
         `;
-    };
-  }
+      }
+      document.querySelector("#cart-total").textContent = `Total : ${total}€`;
+    } else {
+      document.querySelector("#cart-container").innerHTML = `
+      <div id="trips-waiting">
+        <p class="cart-text">No tickets in your cart.</p>
+        <p class="cart-text">Why not plan a trip?</p>
+      </div>
+    `;
+    }
     deleteTrip();
   });
 
@@ -49,8 +59,23 @@ function deleteTrip() {
         .then((data) => {
           if (data.result) {
             this.parentNode.remove();
+
+            let total = 0;
+            document.querySelectorAll(".price").forEach((price) => {
+              total += parseFloat(price.textContent);
+            });
+            document.querySelector("#cart-total").textContent =
+              `Total : ${total}€`;
           }
         });
     });
   });
+}
+
+function purchaseCart() {
+  document
+    .querySelector("#purchase-button")
+    .addEventListener("click", function () {
+      window.location.assign("booking.html");
+    });
 }
